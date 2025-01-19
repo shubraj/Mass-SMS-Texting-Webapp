014971942885bf3174511109d3abcebf9de6196c
Shub Raj Lama <hello@shubraj.com>
2025-01-19 21:36:23 +0545

    feat: privacy and terms added

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from .views import CustomLoginView,DashboardView,CustomLogoutView,ContactView,CompaignView,ConversationView,ConversationDetail,ReceiveSMSView
 app_name = "app_webapp"
 urlpatterns = [
+    path("terms-and-conditions/", TemplateView.as_view(template_name="webapp/terms-and-conditions.html"), name="terms_and_conditions"),
+    path("privacy-policy/", TemplateView.as_view(template_name="webapp/privacy-policy.html"), name="privacy_policy"),
     path('receive-sms/', ReceiveSMSView.as_view(), name='receive_sms'),
     path("conversations/<int:pk>/",ConversationDetail.as_view(), name="conversation_detail"),
     path("conversations/",ConversationView.as_view(),name="conversations"),
